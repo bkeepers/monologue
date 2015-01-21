dacae9f1c1c379bd3f12bdd067e6242d7907ea89
azu <azuciao@gmail.com>
2015-01-22 00:01:39 +0900

    refactor(app): Separating DOM and Ajax
    create `Statuses` class which has `add` and `fetchList`.
    these methods is wrapper of `$.ajax`.

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,30 @@
-function addStatus(options) {
-    var text = options.text;
+function Statuses() {
+
+}
+
+Statuses.prototype.fetchList = function (options) {
+    $.ajax({
+        url: '/statuses',
+        dataType: 'json',
+        success: options.success
+    });
+};
+Statuses.prototype.add = function (options) {
     $.ajax({
         url: '/statuses',
         type: 'POST',
         dataType: 'json',
-        data: {text: text},
+        data: {text: options.text},
         success: options.success
     });
-}
+};
+
+
 jQuery(function ($) {
+    var statuses = new Statuses();
     $('form').on('submit', function (event) {
         event.preventDefault();
-        addStatus({
+        statuses.add({
             text: $(event.target).find("textarea").val(),
             success: function (data) {
                 $('#statuses').append('<li>' + data.text + '</li>');
@@ -19,14 +32,12 @@ jQuery(function ($) {
         });
     });
 
-    $.ajax({
-        url: '/statuses',
-        dataType: 'json',
+    statuses.fetchList({
         success: function (data) {
             var $statuses = $('#statuses');
             for (var i = 0; data.length > i; i++) {
                 $statuses.append('<li>' + data[i].text + '</li>');
             }
         }
-    })
+    });
 });
