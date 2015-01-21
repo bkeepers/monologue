9012b6a6019477007ac45efb4fa743719beb90b8
azu <azuciao@gmail.com>
2015-01-21 23:50:43 +0900

    refactor(app): use preventDefault instead of return false

--- public/js/app.js
+++ public/js/app.js
@@ -1,25 +1,25 @@
-jQuery(function($) {
-  $('form').on('submit', function() {
-    $.ajax({
-      url: '/statuses',
-      type: 'POST',
-      dataType: 'json',
-      data: {text: $(this).find('textarea').val()},
-      success: function(data) {
-        $('#statuses').append('<li>' + data.text + '</li>');
-      }
+jQuery(function ($) {
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        $.ajax({
+            url: '/statuses',
+            type: 'POST',
+            dataType: 'json',
+            data: {text: $(this).find('textarea').val()},
+            success: function (data) {
+                $('#statuses').append('<li>' + data.text + '</li>');
+            }
+        });
     });
-    return false;
-  });
 
-  $.ajax({
-    url: '/statuses',
-    dataType: 'json',
-    success: function(data) {
-      var $statuses = $('#statuses');
-      for(var i = 0; data.length > i; i++) {
-        $statuses.append('<li>' + data[i].text + '</li>');
-      }
-    }
-  })
+    $.ajax({
+        url: '/statuses',
+        dataType: 'json',
+        success: function (data) {
+            var $statuses = $('#statuses');
+            for (var i = 0; data.length > i; i++) {
+                $statuses.append('<li>' + data[i].text + '</li>');
+            }
+        }
+    })
 });
